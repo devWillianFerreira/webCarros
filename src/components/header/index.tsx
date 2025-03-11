47b92d33c95fc7b01bffd8f7282ac28cf08fe9e9
zReBolTz <ferreiraw36@gmail.com>
2025-03-10 21:37:14 -0300

    feat: Criando header

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,7 +1,32 @@
+import { Link } from "react-router-dom";
+import logoImg from "../../assets/logo.svg";
+import { LogInIcon, User } from "lucide-react";
+
 const Header = () => {
+  const signed = true;
+  const loadingAuth = false;
+
   return (
-    <div>
-      <h1>Header</h1>
+    <div className="flex w-full justify-center h-16 items-center bg-white drop-shadow mb-4">
+      <header className="flex justify-between items-center w-full max-w-7xl px-4 mx-auto">
+        <Link to="/">
+          <img src={logoImg} alt="webCarros" />
+        </Link>
+        {!loadingAuth && signed && (
+          <Link
+            to="/dashboard"
+            className="border-2 rounded-full border-gray-900 p-1"
+          >
+            <User size={26} />
+          </Link>
+        )}
+
+        {!loadingAuth && !signed && (
+          <Link to="/login">
+            <LogInIcon size={26} />
+          </Link>
+        )}
+      </header>
     </div>
   );
 };
