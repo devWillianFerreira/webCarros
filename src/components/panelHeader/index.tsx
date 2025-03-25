3fd699b952311abec1dc4153cb7d2193711ab9c8
zReBolTz <ferreiraw36@gmail.com>
2025-03-24 21:55:43 -0300

    feat: Adicionando margin

--- src/components/panelHeader/index.tsx
+++ src/components/panelHeader/index.tsx
@@ -6,14 +6,13 @@ const DashboardHeader = () => {
     await supabase.auth.signOut();
   }
   return (
-
-      <div className="bg-red-500 flex items-center font-medium gap-4 rounded-lg h-11 px-4 text-white">
-        <Link to="/dashboard">Dashboard</Link>
-        <Link to="/dashboard/newCar">Cadastrar Carro</Link>
-        <button onClick={handleLogout} className="ml-auto cursor-pointer">
-          Sair da conta
-        </button>
-      </div>
+    <div className="bg-red-500 flex items-center font-medium gap-4 rounded-lg h-11 px-4 mb-4 text-white">
+      <Link to="/dashboard">Dashboard</Link>
+      <Link to="/dashboard/newCar">Cadastrar Carro</Link>
+      <button onClick={handleLogout} className="ml-auto cursor-pointer">
+        Sair da conta
+      </button>
+    </div>
   );
 };
 
