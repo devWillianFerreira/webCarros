91e1f942882da1d36857da5cb527555e972fc00d
zReBolTz <ferreiraw36@gmail.com>
2025-03-25 21:33:55 -0300

    feat: criando página de detalhes

--- src/pages/carDetail/index.tsx
+++ src/pages/carDetail/index.tsx
@@ -1,8 +1,110 @@
+import { useParams } from "react-router-dom";
+import Container from "../../components/container";
+import { useEffect, useState } from "react";
+import { supabase } from "../../services/supabaseConnection";
+import { Phone } from "lucide-react";
+
+interface carProps {
+  id: string;
+  name: string;
+  year: string;
+  model: string;
+  km: string;
+  price: string | number;
+  city: string;
+  description: string;
+  whatsapp: string;
+  images: imageCarsProps[];
+  uidUser: string;
+  created: string;
+  owner: string;
+}
+interface imageCarsProps {
+  uid: string;
+  name: string;
+  url: string;
+}
+
 const CarDetail = () => {
+  const { id } = useParams();
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const [car, setCar] = useState<carProps>();
+
+  useEffect(() => {
+    async function loadCar() {
+      try {
+        //Buscando carro pelo id
+        // .single() significa que será retornado apenas uma linha da tabela
+        const { data, error } = await supabase
+          .from("car")
+          .select("*")
+          .eq("id", id)
+          .single();
+        if (error) {
+          console.error("Erro na Busca: ", error);
+          return;
+        }
+        setCar({
+          id: data.id,
+          city: data.city,
+          name: data.name,
+          model: data.model,
+          km: data.km,
+          year: data.year,
+          price: data.price,
+          description: data.description,
+          whatsapp: data.whatsapp,
+          uidUser: data.uidUser,
+          owner: data.owner,
+          created: data.created,
+          images: data.images,
+        });
+      } catch (error) {
+        console.log(error);
+      }
+    }
+    loadCar();
+  }, [id]);
+
   return (
-    <div>
-      <h1>Pagina de Car detail</h1>
-    </div>
+    <Container>
+      <h1>Slider</h1>
+      {car && (
+        <main className="w-full bg-white rounded-lg p-6 my-4">
+          <div className="flex flex-col items-center   sm:flex-row mb-4 justify-between font-bold text-3xl text-black ">
+            <h1>{car?.name}</h1>
+            <h1>R$ {car?.price}</h1>
+          </div>
+          <p>{car?.model}</p>
+          <div className="flex w-full gap-6 my-4">
+            <div className="flex flex-col gap-4">
+              <div>
+                <p>Cidade</p>
+                <strong>{car?.city}</strong>
+              </div>
+              <div>
+                <p>Ano</p>
+                <strong>{car?.year}</strong>
+              </div>
+            </div>
+            <div className="flex flex-col gap-4">
+              <div>
+                <p>Km</p>
+                <strong>{car?.km}</strong>
+              </div>
+            </div>
+          </div>
+          <strong>Descrição:</strong>
+          <p className="mb-4">{car?.description}</p>
+          <strong>Telefone:</strong>
+          <p>{car?.whatsapp}</p>
+          <a className="w-full bg-green-500 text-white flex items-center justify-center rounded-lg gap-2 my-6 h-11 text-xl font-medium cursor-pointer">
+            <Phone color="white" />
+            Enviar mensagem ao vendendor
+          </a>
+        </main>
+      )}
+    </Container>
   );
 };
 
